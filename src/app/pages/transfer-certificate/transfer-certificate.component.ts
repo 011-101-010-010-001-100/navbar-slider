57becf4e3c352d2bd29fc52947c9cd8014617df1
Subrat Tiwari <trasmtiwari46833@gmail.com>
2024-10-03 23:04:05 +0530

    Initial commit

---
+++ src/app/pages/transfer-certificate/transfer-certificate.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-transfer-certificate',
+  standalone: true,
+  imports: [],
+  templateUrl: './transfer-certificate.component.html',
+  styleUrl: './transfer-certificate.component.css'
+})
+export class TransferCertificateComponent {
+
+}
